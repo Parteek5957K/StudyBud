4267112cf25890d9ebc37adcecfd81ca6e7ccde7
Guryansh Singla <108322368+Guryansh@users.noreply.github.com>
2024-06-10 18:41:24 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,3 +4,10 @@ apt-get install -y python3-pip python3-venv
 python3 -m venv myenv
 source myenv/bin/activate
 pip install -r requirements.txt
+
+# Run Django collectstatic to gather static files
+python manage.py collectstatic
+
+# Move the collected static files to the expected directory
+mkdir -p staticfiles_build
+mv static staticfiles_build/static
