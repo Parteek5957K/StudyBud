ce4074516b0b326490ad76d4db6a6e27f71cb5b8
Guryansh Singla <108322368+Guryansh@users.noreply.github.com>
2024-06-10 17:23:05 +0530

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mychat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'QqkPkulIHIwtnWRhHHgStdNAVulLWJDJ',
+        'HOST':'viaduct.proxy.rlwy.net',
+        'PORT':'26173',
     }
 }
 
